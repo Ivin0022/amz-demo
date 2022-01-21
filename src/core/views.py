387e7527db93022e9f8db224b1573cfb0874a48a
ivin stephen <ivinstephen001@gmail.com>
2022-01-22 04:17:11 +0530

    added Viewset options

--- src/core/views.py
+++ src/core/views.py
@@ -1,16 +1,24 @@
 # builtins
 from dataclasses import asdict, dataclass
-from typing import Union
+from typing import Union, Callable
 
 # django
 from django.apps import apps
 from django.db.models.options import Options
 from django.db import models
 
+# django filters
+from django_filters.rest_framework import FilterSet
+
 # rest framework
+from rest_framework.pagination import BasePagination
+from rest_framework.permissions import BasePermission
 from rest_framework.routers import DefaultRouter
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
+
+FIELDS_OR_ALL = Union[str, list, tuple]
+FIELDS = Union[list, tuple]
 
 VIEWSET_OPTIONS = [
     'pagination_class',
@@ -30,28 +38,44 @@ VIEWSET_OPTIONS = [
 class BaseOptions:
     model: models.Model
 
-    def to_dict(self, opts: dict) -> dict:
-        return {k: opts.get(k) or v for k, v in asdict(self).items()}
+    def to_dict(self) -> dict:
+        API = getattr(self.model, 'API', None)
+        options: dict = getattr(API, '__dict__', {})
+        options = {k: options.get(k) or v for k, v in asdict(self).items()}
+
+        # filter None values in dict
+        return {k: v for k, v in options.items() if v is not None}
 
 
 @dataclass
 class SerializerOpitons(BaseOptions):
-    fields: Union[str, list, tuple] = '__all__'
-    read_only_fields: Union[list, tuple] = None
+    fields: FIELDS_OR_ALL = '__all__'
+    read_only_fields: FIELDS = None
     depth: int = None
 
 
-class ViewSetOpitons:
+@dataclass
+class ViewSetOpitons(BaseOptions):
 
-    def __init__(self, model) -> None:
-        self.model = model
+    queryset: models.QuerySet = None
+    pagination_class: list[BasePagination] = None
+    permission_classes: list[BasePermission] = None
+    filterset_class: FilterSet = None
+    filterset_fields: FIELDS = None
+    search_fields: FIELDS = None
+    ordering_fields: FIELDS = None
+
+    get_queryset: Callable = None
+    get_serializer_class: Callable = None
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
         self.meta: Options = self.model._meta
         self.fields = self.meta.fields
-        self.fk_fields = [field for field in self.fields if isinstance(field, models.ForeignKey)]
-        self.field_names = [field.name for field in self.fields]
 
     def _get_field_names_for_type(self, types):
-        return [field.name for field in self.fields if isinstance(field, types)]
+        return [field.name for field in self._get_fields_for_type(types)]
 
     def _get_fields_for_type(self, types):
         return [field for field in self.fields if isinstance(field, types)]
@@ -78,25 +102,23 @@ class ViewSetOpitons:
         types = (models.DateField, models.DateTimeField)
         return self._get_field_names_for_type(types)
 
+    @property
+    def queryset(self):
+        return self.model._default_manager.all()
+
 
 class ModelAPI:
 
     def __init__(self, model) -> None:
         self.model: models.Model = model
         self.meta: Options = self.model._meta
+
         self.api_name: str = self.meta.verbose_name_plural.lower().replace(' ', '/')
-        self.basename: str = self.api_name
         self.model_name: str = self.meta.model_name.title()
-        self.API = getattr(self.model, 'API', None)
-        self.options: dict = getattr(self.API, '__dict__', {})
-        self.serializer_options = SerializerOpitons(self.model).to_dict(self.options)
+        self.basename: str = self.api_name
 
-    def get_viewset_options(self):
-
-        default_options = ViewSetOpitons(self.model)
-        default_options = {k: getattr(default_options, k) for k in VIEWSET_OPTIONS if hasattr(default_options, k)}
-
-        return {**default_options, **self.options}
+        self.serializer_options = SerializerOpitons(self.model).to_dict()
+        self.viewset_options = ViewSetOpitons(self.model).to_dict()
 
     def get_serializer_meta_class(self):
         return type(
@@ -118,9 +140,8 @@ class ModelAPI:
             f'{self.model_name}ViewSet',
             (ModelViewSet,),
             dict(
-                queryset=self.model._default_manager.all(),
                 serializer_class=self.get_serializer_class(),
-                **self.get_viewset_options(),
+                **self.viewset_options,
             ),
         )
 
